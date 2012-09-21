a8f81e86bfd74fc681dd318e8a68fa55e7340d1a
felgenhm <felgenh3@uwm.edu>
2012-09-21 14:37:25 -0500

    first edit

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,10 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	    for ( String arg : args )
+		{
+		    System.out.println ( arg );
+		}
 	}
 
 }
