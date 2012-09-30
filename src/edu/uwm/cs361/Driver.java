91dd2a59ab3f93398c262705e391e1594fe76970
felgenhm <felgenh3@uwm.edu>
2012-09-30 15:27:50 -0500

    updated requirements, add lib, update build

--- src/edu/uwm/cs361/Driver.java
+++ src/edu/uwm/cs361/Driver.java
@@ -8,6 +8,15 @@ class Driver
 {
     public static void main ( String[] args )
     {
-	
+	Application app = new SDIApplication();
+	DefaultApplicationModel model = new DefaultApplicationModel();
+
+	model.setName("UMLDiagrammer");
+	model.setVersion("1.0");
+	model.setCopyright("CopyLeft?");
+	model.setViewClassName("edu.uwm.cs361.UMLView");
+
+	app.setModel(model);
+	app.launch(args);
     }
 }
---
+++ src/edu/uwm/cs361/UMLView.java
@@ -0,0 +1,15 @@
+package edu.uwm.cs361;
+
+import org.jhotdraw.app.*;
+
+class UMLView extends AbstractView
+{
+
+    public UMLView ( )
+    {
+
+    }
+
+
+
+}
