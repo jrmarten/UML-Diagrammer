c4dad8b08438bd26cd5f6c5ef440bb31cff76ae1
Tony Klose <anthony.klose@gmail.com>
2012-10-01 14:11:02 -0500

    Changed UMLView access modifier to public, added necessary extended methods.

--- src/edu/uwm/cs361/UMLView.java
+++ src/edu/uwm/cs361/UMLView.java
@@ -1,14 +1,38 @@
 package edu.uwm.cs361;
 
-import org.jhotdraw.app.*;
+import java.io.IOException;
+import java.net.URI;
 
-class UMLView extends AbstractView
+import org.jhotdraw.app.*;
+import org.jhotdraw.gui.URIChooser;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+public class UMLView extends AbstractView
 {
 
     public UMLView ( )
     {
 
     }
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void write(URI uri, @Nullable URIChooser chooser) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read(URI uri, @Nullable URIChooser chooser) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 
