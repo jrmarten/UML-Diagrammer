369ceb229a050cc9ee6b36614010bf41b3b8956c
felgenhm <felgenh3@uwm.edu>
2012-10-05 10:58:24 -0500

    Update View

--- src/edu/uwm/cs361/UMLView.java
+++ src/edu/uwm/cs361/UMLView.java
@@ -1,6 +1,8 @@
 package edu.uwm.cs361;
 
 import java.awt.BorderLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.net.URI;
 
@@ -8,6 +10,8 @@ import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import org.jhotdraw.app.*;
+import org.jhotdraw.app.action.edit.RedoAction;
+import org.jhotdraw.app.action.edit.UndoAction;
 import org.jhotdraw.draw.DefaultDrawing;
 import org.jhotdraw.draw.DefaultDrawingEditor;
 import org.jhotdraw.draw.DefaultDrawingView;
@@ -18,80 +22,93 @@ import org.jhotdraw.undo.UndoRedoManager;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
 
-@SuppressWarnings("serial")
+
 public class UMLView extends AbstractView
 {
-	private JScrollPane scrollpane = new JScrollPane ( );
-	private DefaultDrawingView view = new DefaultDrawingView ( );
-	private DrawingEditor editor;
-	private UndoRedoManager undo;
-	
-	
+    private JScrollPane scrollpane = new JScrollPane ( );
+    private DefaultDrawingView view = new DefaultDrawingView ( );
+    private DrawingEditor editor;
+    private UndoRedoManager undo;
+
+
     public UMLView ( )
     {
-    	initScroll( );
+        initScroll ( );
+	initEditor ( );
     }
-    
+
     public void initEditor ( )
     {
-    	setEditor( new DefaultDrawingEditor());
-    	undo = new UndoRedoManager ( );
-    	view.setDrawing( createDrawing ( ) );
+        setEditor( new DefaultDrawingEditor());
+        undo = new UndoRedoManager ( );
+        view.setDrawing( createDrawing ( ) );
+
+        view.getDrawing().addUndoableEditListener ( undo );
+
+        getActionMap().put ( UndoAction.ID, undo.getUndoAction() );
+        getActionMap().put ( RedoAction.ID, undo.getRedoAction() );
+
+        undo.addPropertyChangeListener ( new PropertyChangeListener() {
+                @Override public void propertyChange ( PropertyChangeEvent e )
+                {
+                    setHasUnsavedChanges ( undo.hasSignificantEdits ( ) );
+                }
+            });
     }
-    
+
     public void initScroll ( )
     {
-    	scrollpane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-    	scrollpane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-    	scrollpane.setViewportView ( view );
-    	setLayout( new BorderLayout( ) );
-    	add ( scrollpane );
-    	
-    	
-    	scrollpane.setLayout(new PlacardScrollPaneLayout());
-    	scrollpane.setBorder ( new EmptyBorder ( 0, 0, 0, 0 ));
+        scrollpane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollpane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollpane.setViewportView ( view );
+        setLayout( new BorderLayout( ) );
+        add ( scrollpane );
+
+
+        scrollpane.setLayout(new PlacardScrollPaneLayout());
+        scrollpane.setBorder ( new EmptyBorder ( 0, 0, 0, 0 ));
     }
-    
-    
+
+
     public void setEditor ( DrawingEditor newEditor)
     {
-    	DrawingEditor old = editor;
-    	if ( old != null ) old.remove(view);
-    	
-    	editor = newEditor;
-    	if ( newEditor != null ) newEditor.add( view );
+        DrawingEditor old = editor;
+        if ( old != null ) old.remove(view);
+
+        editor = newEditor;
+        if ( newEditor != null ) newEditor.add( view );
     }
-    
-    public DefaultDrawing createDrawing ( )
+
+    protected DefaultDrawing createDrawing ( )
     {
-    	DefaultDrawing drawing = new DefaultDrawing ( );
-    	//io blah
-    	return drawing;
+        DefaultDrawing drawing = new DefaultDrawing ( );
+        //io blah
+        return drawing;
     }
-    
+
     public boolean canSaveTo( URI uri )
     {
-    	return uri.getPath().endsWith(".xml");
+        return uri.getPath().endsWith(".xml");
     }
 
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override protected void setHasUnsavedChanges ( boolean newValue)
+    {
+    	super.setHasUnsavedChanges( newValue );
+    	undo.setHasSignificantEdits( newValue );
+    }
+    
+    @Override public void clear() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void write(URI uri, @Nullable URIChooser chooser) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void read(URI uri, @Nullable URIChooser chooser) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override public void write(URI uri, @Nullable URIChooser chooser) throws IOException {
+        // TODO Auto-generated method stub
 
+    }
 
+    @Override public void read(URI uri, @Nullable URIChooser chooser) throws IOException {
+        // TODO Auto-generated method stub
 
+    }
 }
