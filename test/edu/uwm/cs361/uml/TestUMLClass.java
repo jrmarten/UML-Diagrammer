550ab32ec24932389d014f369b9fcb2352aa206d
jrmarten <jrmarten@10.216.248.7>
2012-10-09 11:15:00 -0500

    added test source file, not yet implemented.

---
+++ test/edu/uwm/cs361/uml/TestUMLClass.java
@@ -0,0 +1,24 @@
+package edu.uwm.cs361.uml;
+
+import static org.junit.Assert.*;
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUMLClass extends TestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public static junit.framework.Test suite() { 
+	    return new JUnit4TestAdapter(UMLClass.class); 
+	}
+}
