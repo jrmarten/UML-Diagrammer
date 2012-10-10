66d3ba3542e105aa6fab19bf7f58dd79bd5e25d7
Tony Klose <anthony.klose@gmail.com>
2012-10-10 13:28:36 -0500

    Corrected indentation

--- src/edu/uwm/cs361/UMLApplicationModel.java
+++ src/edu/uwm/cs361/UMLApplicationModel.java
@@ -1,6 +1,5 @@
 package edu.uwm.cs361;
 
-
 import java.util.HashMap;
 
 import org.jhotdraw.app.Application;
@@ -34,145 +33,125 @@ import org.jhotdraw.gui.URIChooser;
 import org.jhotdraw.gui.filechooser.ExtensionFileFilter;
 import org.jhotdraw.util.*;
 
-public class UMLApplicationModel
-extends DefaultApplicationModel
-{
+public class UMLApplicationModel extends DefaultApplicationModel {
 
-    private DefaultDrawingEditor sharedEditor;
-    private HashMap<String, Action> actions;
+	private DefaultDrawingEditor sharedEditor;
+	private HashMap<String, Action> actions;
 
-    public UMLApplicationModel ( ) { }
+	public UMLApplicationModel() {
+	}
 
+	// TODO: FINISH
+	@Override
+	public ActionMap createActionMap(Application a, @Nullable View v) {
+		ActionMap m = super.createActionMap(a, v);
+		ResourceBundleUtil drawLabels = ResourceBundleUtil
+				.getBundle("edu.uwm.cs361.Labels");
+		return m;
 
-    //TODO: FINISH
-    @Override public ActionMap createActionMap ( Application a, @Nullable View v )
-    {
-	ActionMap m = super.createActionMap( a, v );
-	ResourceBundleUtil drawLabels = ResourceBundleUtil.getBundle("edu.uwm.cs361.Labels" );
-	return m;
-	
+	}
 
-    }
-	
+	public DefaultDrawingEditor getSharedEditor() {
+		if (sharedEditor == null)
+			sharedEditor = new DefaultDrawingEditor();
+		return sharedEditor;
+	}
 
+	@Override
+	public java.util.List<JToolBar> createToolBars(Application app,
+			@Nullable View view) {
+		ResourceBundleUtil Labels = ResourceBundleUtil
+				.getBundle("edu.uwm.cs361.Labels");
+		UMLView umlv = (UMLView) view;
 
-    public DefaultDrawingEditor getSharedEditor ( )
-    {
-	if ( sharedEditor == null )
-	    sharedEditor = new DefaultDrawingEditor ( ) ;
-	return sharedEditor;
-    }
+		DrawingEditor edit;
+		edit = (umlv == null) ? getSharedEditor() : umlv.getEditor();
 
-    @Override public java.util.List<JToolBar> createToolBars
-    ( Application app, @Nullable View view)
-    {
-    	ResourceBundleUtil Labels = ResourceBundleUtil.getBundle ( "edu.uwm.cs361.Labels");
-    	UMLView umlv = (UMLView) view;
-    	
-    	DrawingEditor edit;
-    	edit = (umlv == null)?getSharedEditor():umlv.getEditor();
-    	
-    	LinkedList<JToolBar> list = new LinkedList<JToolBar>();
-    	
-    	JToolBar tmp = new JToolBar( );
-    	addCreationButtonsTo(tmp, edit);
-    	tmp.setName( Labels.getString( "window.drawToolBar.title"));
-    	
-    	list.add ( tmp );
-    	
-    	return list;
-    }
-    
+		LinkedList<JToolBar> list = new LinkedList<JToolBar>();
 
-    //NOTE:might not make sense for this application
-    @Override public void initView ( Application a, @Nullable View p )
-    {
-	if ( a.isSharingToolsAmongViews ( ) )
-	    ((UMLView) p ).setEditor( getSharedEditor( ) );
-    }
+		JToolBar tmp = new JToolBar();
+		addCreationButtonsTo(tmp, edit);
+		tmp.setName(Labels.getString("window.drawToolBar.title"));
 
-    @Override protected MenuBuilder createMenuBuilder ( )
-    {
-    	return new UMLMenuBuilder ( );
-    }
+		list.add(tmp);
 
-    private void addCreationButtonsTo ( JToolBar tb, final DrawingEditor edit )
-    {
-	HashMap<AttributeKey, Object> attributes;
+		return list;
+	}
 
-	ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels");
-	//Resource bundles
+	// NOTE:might not make sense for this application
+	@Override
+	public void initView(Application a, @Nullable View p) {
+		if (a.isSharingToolsAmongViews())
+			((UMLView) p).setEditor(getSharedEditor());
+	}
 
-	ButtonFactory.addSelectionToolTo ( tb, edit );
-	tb.addSeparator();
+	@Override
+	protected MenuBuilder createMenuBuilder() {
+		return new UMLMenuBuilder();
+	}
 
-	attributes = new HashMap<AttributeKey, Object> ( );
-	attributes.put(AttributeKeys.FILL_COLOR, Color.white );
-	attributes.put(AttributeKeys.STROKE_COLOR, Color.black);
-        attributes.put(AttributeKeys.TEXT_COLOR, Color.black);
+	private void addCreationButtonsTo(JToolBar tb, final DrawingEditor edit) {
+		HashMap<AttributeKey, Object> attributes;
 
-	ButtonFactory.addToolTo ( tb, edit, new CreationTool ( new ClassFigure(), attributes),
-				  "edit.createClass", labels );
-	
-    }
-    
-    @Override public URIChooser
-    createOpenChooser
-    (Application app, @Nullable View v)
-    {
-    	JFileURIChooser c = new JFileURIChooser ( );
-    	c.addChoosableFileFilter(new ExtensionFileFilter ( "UML Diagram", "xml"));
-    	return c;
-    }
+		ResourceBundleUtil labels = ResourceBundleUtil
+				.getBundle("org.jhotdraw.draw.Labels");
+		// Resource bundles
 
-    @Override public URIChooser 
-    createSaveChooser
-    (Application a, @Nullable View v) 
-    {
-        JFileURIChooser c = new JFileURIChooser ( );
-        c.addChoosableFileFilter(new ExtensionFileFilter("UML Diagram", "xml"));
-        return c;
-    }
+		ButtonFactory.addSelectionToolTo(tb, edit);
+		tb.addSeparator();
 
-    private static class
-    UMLMenuBuilder
-    extends DefaultMenuBuilder
-    {
-    	@Override
-		public void
-    	addOtherViewItems
-    	( JMenu menu, Application app, @Nullable View view )
-    	{
-    		ActionMap am = app.getActionMap ( view );
-    			JCheckBoxMenuItem check;
-    			check = new JCheckBoxMenuItem ( am.get("view.toggleGrid") );
-    			ActionUtil.configureJCheckBoxMenuItem(check,
-    					am.get( "view.toggleGrid" ) );
-    			menu.add(check);
-    	}
-    	
-    }
-    
-    private static class ToolButtonListener 
-    implements ItemListener
-    {
-    	private Tool tool;
-    	private DrawingEditor editor;
+		attributes = new HashMap<AttributeKey, Object>();
+		attributes.put(AttributeKeys.FILL_COLOR, Color.white);
+		attributes.put(AttributeKeys.STROKE_COLOR, Color.black);
+		attributes.put(AttributeKeys.TEXT_COLOR, Color.black);
 
-    	public ToolButtonListener ( Tool t, DrawingEditor e )
-    	{
-    		tool = t;
-    		editor = e;
-    	}
+		ButtonFactory.addToolTo(tb, edit, new CreationTool(new ClassFigure(),
+				attributes), "edit.createClass", labels);
 
-    	@Override public void
-    	itemStateChanged 
-    	( ItemEvent e )
-    	{
-    		if ( e.getStateChange ( ) == ItemEvent.SELECTED ) 
-    		{
-    			editor.setTool ( tool );
-    		}
-    	}
-    }
+	}
+
+	@Override
+	public URIChooser createOpenChooser(Application app, @Nullable View v) {
+		JFileURIChooser c = new JFileURIChooser();
+		c.addChoosableFileFilter(new ExtensionFileFilter("UML Diagram", "xml"));
+		return c;
+	}
+
+	@Override
+	public URIChooser createSaveChooser(Application a, @Nullable View v) {
+		JFileURIChooser c = new JFileURIChooser();
+		c.addChoosableFileFilter(new ExtensionFileFilter("UML Diagram", "xml"));
+		return c;
+	}
+
+	private static class UMLMenuBuilder extends DefaultMenuBuilder {
+		@Override
+		public void addOtherViewItems(JMenu menu, Application app,
+				@Nullable View view) {
+			ActionMap am = app.getActionMap(view);
+			JCheckBoxMenuItem check;
+			check = new JCheckBoxMenuItem(am.get("view.toggleGrid"));
+			ActionUtil.configureJCheckBoxMenuItem(check,
+					am.get("view.toggleGrid"));
+			menu.add(check);
+		}
+
+	}
+
+	private static class ToolButtonListener implements ItemListener {
+		private Tool tool;
+		private DrawingEditor editor;
+
+		public ToolButtonListener(Tool t, DrawingEditor e) {
+			tool = t;
+			editor = e;
+		}
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			if (e.getStateChange() == ItemEvent.SELECTED) {
+				editor.setTool(tool);
+			}
+		}
+	}
 }
