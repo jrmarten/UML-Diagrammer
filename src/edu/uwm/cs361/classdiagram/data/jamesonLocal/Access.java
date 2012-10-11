cff2465abf093a4b94841b225b4cd53f27767796
jrmarten <jrmarten@192.168.0.198>
2012-10-11 14:45:22 -0500

    When I pulled the files I got all sorts of repository errors.  I tried to push 2 files, and have no idea what the hell happened.
    I moved all of my local files to a different package.

---
+++ src/edu/uwm/cs361/classdiagram/data/jamesonLocal/Access.java
@@ -0,0 +1,53 @@
+package edu.uwm.cs361.classdiagram.data.jamesonLocal;
+
+public enum Access
+{
+	PRIVATE ( "private", '-' ),
+	PUBLIC ( "public", '+' ),
+	DEFAULT ( "default", '~' ),
+	PROTECTED ("protected", '#');
+
+
+	private String name = "";
+	private char sym = 0;
+
+	private Access ( String str, char symbol )
+	{
+		name = str;
+		sym = symbol;
+	}
+
+	public char getSymbol ( )
+	{
+		return sym;
+	}
+
+	public static Access fromSymbol ( char c )
+	{
+		switch ( c )
+		{
+			case '+': return Access.PUBLIC;
+			case '-': return Access.PRIVATE;
+			case '#': return Access.PROTECTED;
+			case '~': return Access.DEFAULT;
+			default: return Access.DEFAULT;
+		}
+	}
+
+
+
+	public static Access fromString ( String perm )
+	{
+		perm = perm.trim();
+		if ( perm.length() == 1 ) return fromSymbol ( perm.charAt ( 0 ) );
+		if ( perm.equalsIgnoreCase ( "public" ) ) return PUBLIC;
+		if ( perm.equalsIgnoreCase ( "protected" ) ) return PROTECTED;
+		if ( perm.equalsIgnoreCase ( "private" ) ) return PRIVATE;
+		return DEFAULT;
+	}
+
+	public String toString()
+	{
+		return name;
+	}
+}
