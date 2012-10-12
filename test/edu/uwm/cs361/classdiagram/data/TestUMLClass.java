06514004aafc82185aa2b43675ab6f5f1b57f0dd
jrmarten <jrmarten@10.216.250.64>
2012-10-12 13:37:31 -0500

    Deleted old TestUMLClass from unused package Updated TestUMLClass in correct package

--- test/edu/uwm/cs361/classdiagram/data/TestUMLClass.java
+++ test/edu/uwm/cs361/classdiagram/data/TestUMLClass.java
@@ -14,7 +14,8 @@ public class TestUMLClass {
 	public void setUp() throws Exception {
 		umlClass = new UMLClass();
 	}
-
+	
+	
 	@Test
 	public void testAddAttribute(){
 		String str = "- size : int";
@@ -22,21 +23,71 @@ public class TestUMLClass {
 		assertTrue(a != null);
 		System.out.println ( a );
 		umlClass.addAttribute(a);
-
-
-
-		//not going to work.
-		//assertTrue((umlClass.getAttributes().toString().contains(str)));
 		assertTrue ( umlClass.getAttributes().contains(a));
 	}
 
 	@Test
 	public void testRemoveAttribute() {
-
+		String str = "- size : int";
+		Attribute a = Attribute.Create(str);
+		assertTrue(a != null);
+		System.out.println ( a );
+		umlClass.addAttribute(a);
+		assertTrue ( umlClass.getAttributes().contains(a));
+		
+		
+		umlClass.removeAttribute(a);
+		assertFalse ( umlClass.getAttributes().contains(a));
+		
 	}
 
 	@Test
 	public void testRenameAttribute(){
-
+		String str = "- size : int";
+		Attribute a = Attribute.Create(str);
+		assertTrue(a != null);
+		System.out.println ( a );
+		umlClass.addAttribute(a);
+		assertTrue ( umlClass.getAttributes().contains(a));
+		
+		String str2 = "- size : double";
+		Attribute b = Attribute.Create(str2);
+		assertTrue(b != null);
+		System.out.println( b );
+		umlClass.renameAttribute(a, b);	
+	
+		assertFalse( umlClass.getAttributes().contains(a));
+		assertTrue( umlClass.getAttributes().contains(b));
+	
+		
 	}
+	
+	@Test
+	public void testAddMethod( ){
+		String str = "+ name():String";
+		Method meth1 = Method.Create( str );
+		System.out.println(meth1);
+		
+		
+	}
+	
+	@Test
+	public void removeMethod( ) {
+		
+	}
+	
+	@Test
+	public void renameMethod( ) {
+		
+	}
+	
+	@Test
+	public void testGetNumberOfMethods( ) {
+		
+	
+	}
+	
+	
+	
+	
 }
