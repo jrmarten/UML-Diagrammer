eb6b932127369edac0cb36b2671f07dcb0b72541
Tony Klose <anthony.klose@gmail.com>
2012-10-16 12:10:11 -0500

    Edited UMLFactory.java. please look over it

--- src/edu/uwm/cs361/UMLFactory.java
+++ src/edu/uwm/cs361/UMLFactory.java
@@ -1,13 +1,3 @@
-/*
- * @(#)PertFactory.java
- *
- * Copyright (c) 1996-2010 by the original authors of JHotDraw and all its
- * contributors. All rights reserved.
- *
- * You may not use, copy or modify this file, except in compliance with the 
- * license agreement you entered into with the copyright holders. For details
- * see accompanying license terms.
- */
 
 package edu.uwm.cs361;
 
@@ -18,11 +8,13 @@ import org.jhotdraw.draw.decoration.ArrowTip;
 import org.jhotdraw.samples.pert.figures.*;
 import org.jhotdraw.draw.*;
 import org.jhotdraw.xml.*;
-//TODO: Change this stuff to be UML related instead of TaskFigure related...
+
+import edu.uwm.cs361.classdiagram.ClassFigure;
+//TODO: Change this stuff to be UML related instead of Pert related...
 public class UMLFactory extends DefaultDOMFactory {
     private final static Object[][] classTagArray = {
-        { DefaultDrawing.class, "PertDiagram" },
-        { TaskFigure.class, "task" },
+        { DefaultDrawing.class, "UMLDiagram" },
+        { ClassFigure.class, "class" },
         { DependencyFigure.class, "dep" },
         { ListFigure.class, "list" },
         { TextFigure.class, "text" },
