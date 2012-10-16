c30f3d3321cf780f0c7ab07e623c32ec51de4d08
felgenhm <felgenh3@uwm.edu>
2012-10-16 12:51:16 -0500

    Util

--- src/edu/uwm/cs361/Util.java
+++ src/edu/uwm/cs361/Util.java
@@ -1,13 +1,35 @@
 package edu.uwm.cs361;
 
+import java.util.Iterator;
 
 public class Util
 {
 
-		public static Object report ( String line )
-		{
-			System.out.println ( line );
-			return null;
-		}
-	
+	private static final boolean	DEBUG	= false;
+
+	public static void dprint ( String line )
+	{
+		if ( DEBUG ) System.out.println ( line );
+	}
+
+	public static <E> String join ( Iterable<E> col, String sep )
+	{
+		Iterator<E> it = col.iterator ( );
+		String result = "";
+
+		while ( it.hasNext ( ) )
+			{
+				result += it.next ( ).toString ( );
+				if ( !it.hasNext ( ) ) break;
+				result += sep;
+			}
+		return result;
+	}
+
+	public static Object report ( String line )
+	{
+		System.out.println ( line );
+		return null;
+	}
+
 }
