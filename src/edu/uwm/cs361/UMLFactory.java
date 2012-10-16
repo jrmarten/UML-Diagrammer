2cdf8b42d21c882032f9e2196e192a617a4fdbee
Tony Klose <anthony.klose@gmail.com>
2012-10-16 12:03:11 -0500

    Added a UMLFactory class - it needs to be updated.

---
+++ src/edu/uwm/cs361/UMLFactory.java
@@ -0,0 +1,45 @@
+/*
+ * @(#)PertFactory.java
+ *
+ * Copyright (c) 1996-2010 by the original authors of JHotDraw and all its
+ * contributors. All rights reserved.
+ *
+ * You may not use, copy or modify this file, except in compliance with the 
+ * license agreement you entered into with the copyright holders. For details
+ * see accompanying license terms.
+ */
+
+package edu.uwm.cs361;
+
+import org.jhotdraw.draw.locator.RelativeLocator;
+import org.jhotdraw.draw.connector.LocatorConnector;
+import org.jhotdraw.draw.connector.ChopRectangleConnector;
+import org.jhotdraw.draw.decoration.ArrowTip;
+import org.jhotdraw.samples.pert.figures.*;
+import org.jhotdraw.draw.*;
+import org.jhotdraw.xml.*;
+//TODO: Change this stuff to be UML related instead of TaskFigure related...
+public class UMLFactory extends DefaultDOMFactory {
+    private final static Object[][] classTagArray = {
+        { DefaultDrawing.class, "PertDiagram" },
+        { TaskFigure.class, "task" },
+        { DependencyFigure.class, "dep" },
+        { ListFigure.class, "list" },
+        { TextFigure.class, "text" },
+        { GroupFigure.class, "g" },
+        { TextAreaFigure.class, "ta" },
+        { SeparatorLineFigure.class, "separator" },
+        
+        { ChopRectangleConnector.class, "rectConnector" },
+        { LocatorConnector.class, "locConnector" },
+        { RelativeLocator.class, "relativeLocator" },
+        { ArrowTip.class, "arrowTip" }
+    };
+    
+    /** Creates a new instance. */
+    public UMLFactory() {
+        for (Object[] o : classTagArray) {
+            addStorableClass((String) o[1], (Class) o[0]);
+        }
+    }
+}
