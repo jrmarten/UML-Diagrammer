5fe9f3d6f930152ca8ce710da8d45684c0cabb88
felgenhm <felgenh3@uwm.edu>
2012-10-23 12:10:34 -0500

    x

---
+++ src/edu/uwm/cs361/UMLDrawingEditor.java
@@ -0,0 +1,20 @@
+package edu.uwm.cs361;
+
+import javax.swing.InputMap;
+
+import org.jhotdraw.draw.DefaultDrawingEditor;
+
+/*
+ * Make hotkeys
+ * not linked to program yet.
+ */
+public class UMLDrawingEditor extends DefaultDrawingEditor
+{
+
+	protected InputMap createInputMap() {
+		InputMap iMaps = super.createInputMap(); // lolz
+
+		return iMaps;
+	}
+
+}
