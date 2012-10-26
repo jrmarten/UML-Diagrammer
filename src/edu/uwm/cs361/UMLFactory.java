4ae21a3111f784ccf3dd8945d57a75f8c0d3adf5
felgenhm <felgenh3@uwm.edu>
2012-10-25 21:45:08 -0500

    reformated

--- src/edu/uwm/cs361/UMLFactory.java
+++ src/edu/uwm/cs361/UMLFactory.java
@@ -1,17 +1,23 @@
 package edu.uwm.cs361;
 
-import org.jhotdraw.draw.locator.RelativeLocator;
-import org.jhotdraw.draw.connector.LocatorConnector;
+import org.jhotdraw.draw.DefaultDrawing;
+import org.jhotdraw.draw.GroupFigure;
+import org.jhotdraw.draw.ListFigure;
+import org.jhotdraw.draw.TextAreaFigure;
+import org.jhotdraw.draw.TextFigure;
 import org.jhotdraw.draw.connector.ChopRectangleConnector;
+import org.jhotdraw.draw.connector.LocatorConnector;
 import org.jhotdraw.draw.decoration.ArrowTip;
-import org.jhotdraw.samples.pert.figures.*;
-import org.jhotdraw.draw.*;
-import org.jhotdraw.xml.*;
+import org.jhotdraw.draw.locator.RelativeLocator;
+import org.jhotdraw.samples.pert.figures.DependencyFigure;
+import org.jhotdraw.samples.pert.figures.SeparatorLineFigure;
+import org.jhotdraw.xml.DefaultDOMFactory;
 
 import com.sun.org.apache.bcel.internal.classfile.Attribute;
 import com.sun.org.apache.bcel.internal.classfile.Method;
 
 import edu.uwm.cs361.classdiagram.ClassFigure;
+import edu.uwm.cs361.classdiagram.InheritanceFigure;
 import edu.uwm.cs361.classdiagram.data.UMLClass;
 
 //TODO: Change this stuff to be UML related instead of Pert related...
@@ -19,13 +25,15 @@ public class UMLFactory extends DefaultDOMFactory
 {
 	private final static Object[][]	classTagArray	= {
 			{ DefaultDrawing.class, "UMLDiagram" },
-			{ ClassFigure.class, "class" },
-
-			{ UMLClass.class, "Class" }, // for lack of a better tag currently
-			{ Method.class, "method" }, { Attribute.class, "attribute" },
-
-			{ DependencyFigure.class, "dep" }, { ListFigure.class, "list" },
-			{ TextFigure.class, "text" }, { GroupFigure.class, "g" },
+			{ ClassFigure.class, "ClassFigure" },
+			{ UMLClass.class, "class" }, // for lack of a better tag currently
+			{ Method.class, "method" }, 
+			{ Attribute.class, "attribute" },
+			{ DependencyFigure.class, "dep" },
+			{ InheritanceFigure.class, "inherit" },
+			{ ListFigure.class, "list" },
+			{ TextFigure.class, "text" },
+			{ GroupFigure.class, "g" },
 			{ TextAreaFigure.class, "ta" },
 			{ SeparatorLineFigure.class, "separator" },
 
