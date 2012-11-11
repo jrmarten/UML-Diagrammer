c929401903f446001cfb805e616b01ee1c216d46
felgenhm <felgenh3@uwm.edu>
2012-11-11 15:44:08 -0600

    added String compatibility

--- src/edu/uwm/cs361/classdiagram/data/ConnectionType.java
+++ src/edu/uwm/cs361/classdiagram/data/ConnectionType.java
@@ -15,17 +15,39 @@ public enum ConnectionType
 	private static final LineDecoration DEFAULT_DECORATOR = null;
 	private static final Object[][] DECORATOR_MAPPINGS = 
 		{
-				{ COMPOSITION, new ArrowTip ( 0.4, 15.0, 30.0 ) },
-				{ AGGREGATION, new ArrowTip ( 0.4, 15.0, 30.0, false, true, true ) },
-				{ ASSOCIATION, new ArrowTip ( .35, 20, 18.4 ) }
+				{ COMPOSITION, new ArrowTip ( 0.4, 15.0, 30.0 ), "Composition" },
+				{ AGGREGATION, new ArrowTip ( 0.4, 15.0, 30.0, false, true, true ), "Aggregation" },
+				{ ASSOCIATION, new ArrowTip ( .35, 20, 18.4 ), "Association" },
+				{ DEPENDANCY, DEFAULT_DECORATOR, "Dependancy" }
 		};
 	
 	public LineDecoration getDecoration ( )
 	{
-		for ( Object[] pair : DECORATOR_MAPPINGS )
+		for ( Object[] info : DECORATOR_MAPPINGS )
 			{
-				if ( this == pair[0] ) return ( LineDecoration ) pair [1];
+				if ( this == info[0] ) return ( LineDecoration ) info[1];
 			}
 		return DEFAULT_DECORATOR;
+	}
+	
+	@Override
+	public String toString()
+	{
+		for ( Object[] info : DECORATOR_MAPPINGS )
+			{
+				if ( this == info[0] ) return (String) info[2];
+			}
+		
+		throw new IllegalStateException ( "ConnectionType is not in Mapping" );
+	}
+	
+	public static ConnectionType parse ( String str )
+	{
+		for ( Object[] info : DECORATOR_MAPPINGS )
+			{
+				if ( str.equalsIgnoreCase( (String)info[2] ) )
+					return (ConnectionType)info[0];
+			}
+		return null;
 	}
 }
