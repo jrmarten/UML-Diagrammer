0b1fb19260fa5f40ef0b99bbee24082bd199b825
felgenhm <felgenh3@uwm.edu>
2012-11-15 12:24:44 -0600

    added Undoable edit classes

--- src/edu/uwm/cs361/action/AddAttributeAction.java
+++ src/edu/uwm/cs361/action/AddAttributeAction.java
@@ -2,12 +2,11 @@ package edu.uwm.cs361.action;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.undo.AbstractUndoableEdit;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
-import javax.swing.undo.UndoableEdit;
 
 import edu.uwm.cs361.UMLApplicationModel;
-import edu.uwm.cs361.Util;
 import edu.uwm.cs361.classdiagram.ClassFigure;
 
 @SuppressWarnings("serial")
@@ -29,71 +28,27 @@ public class AddAttributeAction extends ClassFigureAction
 
 	
 	
-	public class AddAttributeEdit implements UndoableEdit
+	public static class Edit extends AbstractUndoableEdit
 	{
 		private ClassFigure fig;
 		private String attr_rep;
-		private boolean alive = true;
 		
-		public AddAttributeEdit ( ClassFigure fig, String attr_rep )
+		public Edit ( ClassFigure fig, String attr_rep )
 		{
 			this.fig = fig;
 			this.attr_rep = attr_rep;
 		}
-		
-		@Override
-		public boolean addEdit(UndoableEdit arg0) {
-			return false;
-		}
-		
-		@Override
-		public String toString ( )
-		{
-			return fig.getData().getName() + " + " + attr_rep;
-		}
-
-		@Override
-		public boolean canRedo() { return alive; }
-
-		@Override
-		public boolean canUndo() { return alive; }
-
-		@Override
-		public void die() {
-			fig = null;
-			attr_rep = null;
-			alive = false;
-		}
-
-		@Override
-		public String getPresentationName() { return "Add Attribute undo"; }
-
-		@Override
-		public String getRedoPresentationName() { return ""; }
-
-		@Override
-		public String getUndoPresentationName() { return ""; }
-
-		@Override
-		public boolean isSignificant() { return true; }
 
 		@Override
 		public void redo() throws CannotRedoException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public boolean replaceEdit(UndoableEdit arg0) {
-			// TODO Auto-generated method stub
-			return false;
+			super.redo();
+			fig.addAttribute( attr_rep );
 		}
 
 		@Override
 		public void undo() throws CannotUndoException {
-			Util.dprint( "undoing" );
+			super.undo();
 			fig.removeAttribute( attr_rep );
 		}
-		
 	}
 }
--- src/edu/uwm/cs361/action/AddMethodAction.java
+++ src/edu/uwm/cs361/action/AddMethodAction.java
@@ -2,6 +2,10 @@ package edu.uwm.cs361.action;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+
 import edu.uwm.cs361.UMLApplicationModel;
 import edu.uwm.cs361.classdiagram.ClassFigure;
 
@@ -21,4 +25,33 @@ public class AddMethodAction extends ClassFigureAction
 	public void actionPerformed(ActionEvent e) {
 		data.addMethod(UMLApplicationModel.prompt(method_prompt));
 	}
+
+
+	
+	//TODO:link
+	public static class Edit extends AbstractUndoableEdit
+	{
+		private ClassFigure cfig;
+		private String meth;
+		
+		public Edit ( ClassFigure cfig, String meth )
+		{
+			this.cfig = cfig;
+			this.meth = meth;
+		}
+
+		@Override
+		public void redo() throws CannotRedoException {
+			super.redo();
+			cfig.addMethod( meth );
+		}
+
+		@Override
+		public void undo() throws CannotUndoException {
+			super.undo();
+			cfig.removeMethod( meth );
+		}
+		
+	}
+
 }
