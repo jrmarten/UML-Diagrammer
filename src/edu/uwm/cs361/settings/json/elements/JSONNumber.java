7cb23747340eb3df829d699f455191a6ef1c497f
felgenhm <felgenh3@uwm.edu>
2012-11-26 14:01:46 -0600

    Added equals

--- src/edu/uwm/cs361/settings/json/elements/JSONNumber.java
+++ src/edu/uwm/cs361/settings/json/elements/JSONNumber.java
@@ -1,9 +1,10 @@
 package edu.uwm.cs361.settings.json.elements;
 
+
 public class JSONNumber extends AbstractJSONElement
 {
 
-	double _val;
+	private double _val;
 	
 	public JSONNumber (  String val )
 	{
@@ -15,6 +16,15 @@ public class JSONNumber extends AbstractJSONElement
 		{
 			_val = 0.0;
 		}
+	}
+	
+	@Override
+	public boolean equals ( Object o )
+	{
+		if ( o instanceof JSONNumber ) return _val == ((JSONNumber)o)._val;
+		if ( o instanceof Double ) return _val == ((Double)o).doubleValue();
+		if ( o instanceof Integer ) return _val == ((Integer)o).intValue();
+		return false;
 	}
 	
 	@Override
